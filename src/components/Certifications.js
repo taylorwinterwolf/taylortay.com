dcf592f1a280763fe3e83bdb47f07ff37b1a1a45
Taylor Tay <303winterwolf@gmail.com>
2024-06-18 11:48:50 -0600

    Updated Resume and made some small page edits

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -1,5 +1,4 @@
 import logo from '../assets/reactLogo.png';
-import jsCert from '../assets/javascript.png'
 import basicsCert from '../assets/reactBasics.png'
 import advanceCert from '../assets/advancedReact.png'
 
@@ -15,7 +14,6 @@ export default function Certifications() {
                     <div className='col-12 col-md-8 col-lg-9'>
                         <h4 className='bold mb-0'>Advanced React</h4>
                         <p className='subtext'>Issued By Meta</p>
-                        <p className='subtext'>Issue Date: May 2023</p>
                         <p className='subtext'>Credential ID: 3EZYF25V6Y32</p>
                         <p>In this course, I examined different types of React components and learned various characteristics and when to use them. I dug into more advanced hooks and create some of my own. I looked into building forms with React. I explored component composition and new patterns, such as Higher Order Components and Render Props. I created a web application that consumes API data and got familiar with the most commonly used React framework integrations, tools, and testing techniques.</p>
                         <a href='https://www.coursera.org/account/accomplishments/certificate/3EZYF25V6Y32' target='_blank' rel="noreferrer" className='certDownloadBtn'><button type="button" className="btn btn-primary certDownloadBtn">View Certificate</button></a>
@@ -26,23 +24,11 @@ export default function Certifications() {
                     <div className='col-12 col-md-8 col-lg-9'>
                         <h4 className='bold mb-0'>React Basics</h4>
                         <p className='subtext'>Issued By Meta</p>
-                        <p className='subtext'>Issue Date: April 2023</p>
                         <p className='subtext'>Credential ID: M2AGXRHDAH9P</p>
                         <p>In this course, I was introduced to the powerful JavaScript library React. I used React to build user interfaces for web and mobile applications. I also explored the fundamental concepts that underpin the React library and learned the basic skills required to build a simple, fast, and scalable app.</p>
                         <a href='https://www.coursera.org/account/accomplishments/certificate/M2AGXRHDAH9P' target='_blank' rel="noreferrer" className='certDownloadBtn'><button type="button" className="btn btn-primary certDownloadBtn">View Certificate</button></a>
                     </div>
                     <div className='col-6 col-sm-6 col-md-4 col-lg-3 col-xl-2'><img src={basicsCert} className='img-fluid' alt='Meta Javascript Certification' loading="lazy"/></div>
-                </div>
-                <div className='row justify-content-center certSection'>
-                    <div className='col-12 col-md-8 col-lg-9'>
-                        <h4 className='bold mb-0'>Programming With Javascript</h4>
-                        <p className='subtext'>Issued By Meta</p>
-                        <p className='subtext'>Issue Date: April 2023</p>
-                        <p className='subtext'>Credential ID: PXFHU76N7QK3</p>
-                        <p>This was just a brushup course to stay current with the latest version of Javascript</p>
-                        <a href='https://www.coursera.org/account/accomplishments/certificate/PXFHU76N7QK3' target='_blank' rel="noreferrer" className='certDownloadBtn'><button type="button" className="btn btn-primary certDownloadBtn">View Certificate</button></a>
-                    </div>
-                    <div className='col-6 col-sm-6 col-md-4 col-lg-3 col-xl-2'><img src={jsCert} className='img-fluid' alt='Meta Javascript Certification' loading="lazy"/></div>
                 </div>
             </div>
         </div>
